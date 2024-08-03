8f7f6a5958f2b5373873615f1b78d75167457016
Ramadhanu Kadir Rachono <90473066+Danu710@users.noreply.github.com>
2024-08-03 20:31:45 +0700

    first Commit

---
+++ src/controller/cats.controller.ts
@@ -0,0 +1,23 @@
+import { Controller, Get, HttpCode, Post, Req, Res } from '@nestjs/common';
+
+@Controller('cats')
+export class CatsController {
+  @HttpCode(200)
+  @Get('index')
+  index(@Res() response) {
+    response.json({
+      title: 'hello',
+    });
+  }
+
+  @Get('create')
+  create(@Res({ passthrough: true }) response): string {
+    response.cookie('name', 'danus');
+    return 'hello danus!';
+  }
+
+  @Post('store')
+  store(@Req() request, @Res() response) {
+    response.status(201).json(request.body);
+  }
+}
